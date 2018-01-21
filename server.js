84a40883e3aea7b9f7fc00cc2d989ae26290e220
PaulODonovan <blueprintdesignmail@yahoo.com>
2018-01-21 12:02:16 +0100

    adds helper

--- server.js
+++ server.js
@@ -2,22 +2,33 @@ const express = require('express');
 const hbs = require('hbs');//handlebars templating
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
 	// res.send('<h1>Hello express</h1>');
-	res.send({
+	res.render('home.hbs', {
+		pageTitle : 'Home Page',
 		name: 'Paul',
-		likes: ['music', 'programming', 'cycling']
+		likes: ['music', 'programming', 'cycling'],
+		welcomeMsg : 'Welcome!',
+
 	});
 });
 
 app.get('/about', (req, res) => {
 	// res.send('About page');
 	res.render('about.hbs', {
-		pageTitle : 'About Page',
-		currYear : new Date().getFullYear()
+		pageTitle : 'About Page'
 	});
 });
 app.listen(3000, () => {
